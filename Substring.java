21ef346be5c038e867403402dc98fa433bb99187
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2019-11-15 09:45:38 +0530

    substring from a string

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring
+{
+	public static void main (String[] args) {
+		String s1 = new String("Computer");
+		String s2 = s1.substring(2,4);
+		System.out.println(s2);
+					   }
+	}
